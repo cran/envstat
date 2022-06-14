64e568c220319b881e00c5a0433bc46b3965b882
Mark Sellors <rstats@5vcc.com>
2022-06-14 14:20:02 +0000

    version 0.0.3

--- R/repos_available.R
+++ R/repos_available.R
@@ -1,10 +1,13 @@
 check_cran_mirror <- function(url) {
-  version_url <- paste0(url, "/src/base/VERSION-INFO.dcf")
+  version_url <- paste0(url, "/src/contrib/PACKAGES.gz")
 
   request <- httr2::request(version_url)
 
   result <- tryCatch(
-    httr2::req_perform(request),
+    httr2::req_perform(httr2::req_error(
+      request,
+      is_error = function(resp) FALSE
+    )),
     error = function(e) NULL
   )
   result_body <- tryCatch(
@@ -12,18 +15,24 @@ check_cran_mirror <- function(url) {
     error = function(e) NULL
   )
 
-  if (result$status_code == 200) {
-    TRUE
+  if (is.null(result)) {
+    status <- FALSE
   } else {
-    FALSE
+    if (result$status_code == 200) {
+      status <- TRUE
+    } else {
+      status <- FALSE
+    }
   }
+
+  status
 }
 
 
 check_repos_available <- function(config, silent = FALSE) {
   cli_silencer(
     silent, "cli_h2",
-    "Checking configured repos are available"
+    "Checking configured package repos are available"
   )
   output <- c()
   if ((is.null(config$repos_available)) || (isFALSE(config$repos_available))) {
@@ -33,7 +42,7 @@ check_repos_available <- function(config, silent = FALSE) {
     )
     output <- append(output, TRUE)
   } else {
-    for (repo in options("repos")) {
+    for (repo in options("repos")$repos) {
       if (repo == "@CRAN@") {
         cli_silencer(
           silent, "cli_alert_danger",
@@ -45,13 +54,13 @@ check_repos_available <- function(config, silent = FALSE) {
         if (isTRUE(repo_up)) {
           cli_silencer(
             silent, "cli_alert_success",
-            paste0("CRAN repo availabe: ", repo)
+            paste0("Package repo available: ", repo)
           )
           output <- append(output, TRUE)
         } else {
           cli_silencer(
             silent, "cli_alert_danger",
-            paste0("Error contacting CRAN repo: ", repo)
+            paste0("Error contacting package repo: ", repo)
           )
           output <- append(output, FALSE)
         }
